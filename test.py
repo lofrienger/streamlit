cf866ec31dfe978839c73a2a8d0986c8c03ec4df
lofrienger <andywangmy@gmail.com>
2020-11-26 21:28:42 +0800

    initial test

---
+++ test.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import time
+
+# import cv2 as cv
+
+st.title('Video Analysis with DNN & OpenCV 你好')
+
+# video_file = open('TestVideo.mp4', 'rb')
+
+# video_bytes_0 = video_file.read()
+# st.video(video_bytes_0)
+
+# my_bar = st.progress(0)
+
+# my_btn = st.button('Display')
+
+# if my_btn:
+#     st.write("Clicked!!!")
+#     for percent_complete in range(100):
+#     	time.sleep(0.1)
+#     	my_bar.progress(percent_complete + 1)
+
+# else:
+#     st.write('Not Clicked...')
+
+audio_file = open('MAGOGfriend_audio.mp4', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/mp4')
+
+
+st.success('Success~')
